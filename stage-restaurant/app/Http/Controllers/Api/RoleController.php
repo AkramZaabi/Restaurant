a8eb1b8c3d8426b2a75b6c49256ca3d88b0c2cef
Akram Zaabi <akramzaabi7@gmail.com>
2024-01-19 12:17:26 +0100

    fixed-role-api

--- stage-restaurant/app/Http/Controllers/Api/RoleController.php
+++ stage-restaurant/app/Http/Controllers/Api/RoleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
@@ -14,7 +15,7 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+        return Role::all();
     }
 
     /**
--- stage-restaurant/routes/api.php
+++ stage-restaurant/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route:: apiResource('/users',UserController::class);
+Route:: apiResource('/roles',RoleController::class);
