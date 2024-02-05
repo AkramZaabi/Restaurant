fb5920ef93d8fc0948f8a9374d716b7d0b3e2476
Akram Zaabi <akramzaabi7@gmail.com>
2024-02-05 21:17:07 +0100

    websocket  setup in vue

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,7 +6,20 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { createPinia } from 'pinia'
+import Echo from "laravel-echo"
 const pinia = createPinia()
 axios.defaults.baseURL="http://localhost:8000/api"
 const app = createApp(App)
 app.use(pinia).use(router).mount("#app");
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+   broadcaster: 'pusher',
+   key: "local",
+   wsHost:"127.0.0.1",
+   wsPort: 6001,
+   cluster: "mt1",
+   forceTLS: false,
+   disableStats: true,
+});
